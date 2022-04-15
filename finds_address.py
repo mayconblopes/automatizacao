f493b82e509851f7b0ef3edc033e477d5b89ff39
Lopes, Maycon B <lopes.maycon@gmail.com>
2022-04-15 18:52:37 -0300

    Retirada estrutura FOR e adicionado loop infinito (encerra quando o usuário fecha o terminal)

--- finds_address.py
+++ finds_address.py
@@ -4,14 +4,15 @@ Finds the complete address of a given Brazilian zip code.
 
 import requests
 
-cep = str(input(f"CEP: "))
+while True:
+    cep = str(input(f"\nCEP: "))
 
-try:
-    r = requests.get(f"http://viacep.com.br/ws/{cep}/json")
-    endereco_completo = r.json()
+    try:
+        r = requests.get(f"http://viacep.com.br/ws/{cep}/json")
+        endereco_completo = r.json()
+        print(f"Endereço: {endereco_completo['logradouro'].upper()}, "
+              f"{endereco_completo['bairro'].upper()}, "
+              f"{endereco_completo['localidade'].upper()}/{endereco_completo['uf'].upper()}.")
 
-    for item in endereco_completo:
-        print(
-            f"{item}: {endereco_completo[item]}") if item == "localidade" or item == "logradouro" or item == "bairro" else None
-except:
-    print("CEP inválido")
+    except:
+        print("CEP inválido")
