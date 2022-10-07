73a68c80e524bb7c21a9f271eac9b033adc3e5eb
mayconblopes <lopes.maycon@gmail.com>
2022-10-07 10:49:23 -0300

    Implemented  'gerar_contrato_honorarios'

--- castro_docs/auto_docs.py
+++ castro_docs/auto_docs.py
@@ -1,12 +1,28 @@
-from ast import Str
 from typing import Dict
 import flet
-from flet import Page, ElevatedButton, TextField, Column, Row, UserControl, AppBar, Text, View, colors
+from flet import Page, ElevatedButton, TextField, Column, Row, UserControl, AppBar, Text, View, colors, Dropdown, dropdown
 from docxtpl import DocxTemplate
 
 
-class ClienteForm(UserControl):
+class DocForm(UserControl):
     def build(self):
+
+        # START - constantes
+        self.TEMPLATE_PROCURACAO: str = 'templates/procuracao.docx'
+        self.TEMPLATE_DECLARACAO_HIPOSSUFICIENCIA: str = 'templates/declaracao_hipossuficiencia.docx'
+        self.TEMPLATE_CONTRATO_HONORARIOS: str ='templates/contrato_honorarios.docx'
+        self.OPCOES_HONORARIOS = {
+            '30% ao final': '30% do aproveitamento econômico somente ao final do processo',
+            '25% ao final': '25% do aproveitamento econômico somente ao final do processo',
+            '20% ao final': '20% do aproveitamento econômico somente ao final do processo',
+            '15% ao final': '15% do aproveitamento econômico somente ao final do processo',
+            '10% ao final': '10% do aproveitamento econômico somente ao final do processo',
+            'Outro': None,
+        }
+        # END - constantes
+
+
+        # START -- campos comuns à Procuracao, Declaracao de Hip. e Contrato de Honorários --
         self.nome_cliente: TextField = TextField(label='NOME DO CLIENTE', width=300, autofocus=True)
         self.nacionalidade: TextField = TextField(label='NACIONALIDADE', width=160)
         self.estado_civil: TextField = TextField(label='ESTADO CIVIL', width=150)
@@ -22,12 +38,18 @@ class ClienteForm(UserControl):
         self.cep: TextField = TextField(label='CEP', width=100)
         self.email: TextField = TextField(label='E-MAIL', width=150)
         self.finalidade: TextField = TextField(label='FINALIDADE', width=630, multiline=True)
-        self.context: Dict 
-        self.template_procuracao: Str = 'templates/procuracao.docx'
-        self.template_declaracao_hipossuficiencia: Str = 'templates/declaracao_hipossuficiencia.docx'
-        self.template_contrato_honorarios: Str ='templates/contrato_honorarios.docx'
+        # END -- campos comuns à Procuracao, Declaracao de Hip. e Contrato de Honorários --
 
-        root =  Column([
+        # START -- campos específicos para o Contrato de honorários --
+        self.administrador_contrato: TextField = TextField(label='ADMINISTRADOR', hint_text='Nome do advogado administrador do contrato de honorários', width=300)
+        self.honorarios: Dropdown = Dropdown(label='HONORÁRIOS', width=200, on_change=self.custom_hono)
+        self.custom_honorarios: TextField = TextField(label='Descrição dos honorários', visible=False)
+        for opcao in self.OPCOES_HONORARIOS.keys():
+            self.honorarios.options.append(dropdown.Option(opcao))
+        # END -- campos específicos para o Contrato de honorários --
+        
+        self.context: Dict 
+        self.root =  Column([
 
                     Row([
                         self.nome_cliente, self.nacionalidade, self.estado_civil, 
@@ -38,44 +60,45 @@ class ClienteForm(UserControl):
                     ], wrap=True),
 
                     Row([
-                        self.logradouro, self.numero, self.bairro
+                        self.logradouro, self.numero, self.bairro,
                     ], wrap=True),
 
                     Row([
-                        self.cidade, self.uf, self.cep, self.email
+                        self.cidade, self.uf, self.cep, self.email,
                     ], wrap=True),
 
                     Row([self.finalidade], wrap=True),
+                    
+                    Row([
+                        self.administrador_contrato, self.honorarios,
+                    ], wrap=True),
+                    
+                    self.custom_honorarios,
 
                     Row([
                         ElevatedButton('Limpar campos', on_click=self.limpar_campos, bgcolor=colors.RED_ACCENT_100, color='black'),
                         ElevatedButton('Nova Procuração', on_click=self.gerar_procuracao),
                         ElevatedButton('Nova Declaração de Hip.', on_click=self.gerar_declaracao_hipossuficiencia),
-                    ])
+                        ElevatedButton('Novo Contrato de Honorários.', on_click=self.gerar_contrato_honorarios),
+                    ]),
+                    
                 ])
         
-        return root
+        return self.root
     
     def limpar_campos(self, e):
-            self.nome_cliente.value = ''
-            self.nacionalidade.value = ''
-            self.estado_civil.value = ''
-            self.profissao.value = ''
-            self.data_nascimento.value = ''
-            self.rg.value = ''
-            self.cpf.value = ''
-            self.logradouro.value = ''
-            self.numero.value = ''
-            self.bairro.value = ''
-            self.cidade.value = ''
-            self.uf.value = ''
-            self.cep.value = ''
-            self.email.value = ''
-            self.finalidade.value = ''
-            self.update()
+        # self.nome_cliente.value = ''
+        # self.nacionalidade.value = ''
+        # (...)
+        # empty all fields with FOR
+        for control in self.root.controls:
+            for field in control.controls:
+                field.value = ''
+        
+        self.update()
 
     def gerar_doc(self, tipo, template):
-      # doc = DocxTemplate('templates/procuracao.docx')
+        # template is a string like 'templates/procuracao.docx'
         doc = DocxTemplate(template)
 
         self.context = { 
@@ -95,6 +118,8 @@ class ClienteForm(UserControl):
             'cep': self.cep.value,
             'email': self.email.value,
             'finalidade': self.finalidade.value,
+            'administrador_contrato': self.administrador_contrato.value,
+            'honorarios': self.custom_honorarios.value or self.OPCOES_HONORARIOS[self.honorarios.value]
         }    
 
         doc.render(self.context)
@@ -102,15 +127,22 @@ class ClienteForm(UserControl):
         doc.save(f'{tipo}_{primeiro_nome}.docx')
     
     def gerar_procuracao(self, e):
-        self.gerar_doc(tipo='Procuracao', template='templates/procuracao.docx')
+        self.gerar_doc(tipo='Procuracao', template=self.TEMPLATE_PROCURACAO)
     
     def gerar_declaracao_hipossuficiencia(self, e):
-        self.gerar_doc(tipo='Declaracao_Hipossuficiencia', template='templates/declaracao _hipossuficiencia.docx')
+        self.gerar_doc(tipo='Declaracao_Hipossuficiencia', template=self.TEMPLATE_DECLARACAO_HIPOSSUFICIENCIA)
 
+    def gerar_contrato_honorarios(self, e):
+        self.gerar_doc(tipo='Contrato_Honorarios', template=self.TEMPLATE_CONTRATO_HONORARIOS)
 
+    def custom_hono(self, e):
+        if self.honorarios.value == 'Outro':
+            self.custom_honorarios.visible = True
+        else:
+            self.custom_honorarios.visible = False
+            self.custom_honorarios.value = None
+        self.update()
 
-
-    
 
 def main(page: Page):
     
@@ -124,8 +156,8 @@ def main(page: Page):
         page.views.append(
             
             View('/', [
-                AppBar(title=Text('Sistema gerador de documentos'), bgcolor=colors.BLUE_GREY_900),
-                ClienteForm()
+                AppBar(title=Text('AutoDocs'), bgcolor=colors.BLUE_GREY_900),
+                DocForm()
             ]),
         )
 
